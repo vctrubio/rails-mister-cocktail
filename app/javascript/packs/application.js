a29a2491a58dfb294f84c1e73b371d8b685b25ad
Victor Rubio <vctrubio@gmail.com>
2019-03-14 12:17:32 -0600

    hi x3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,11 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+
+
+import 'bootstrap';
+
 console.log('Hello World from Webpacker')
+
+
+
